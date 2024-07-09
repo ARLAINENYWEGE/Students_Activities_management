afd70811f98bb4df6bfe273e42b077a3cd8c128e
lynndede <167207904+lynndede@users.noreply.github.com>
2024-07-09 14:19:27 +0300

    Update part 1.cpp

--- part 1.cpp
+++ part 1.cpp
@@ -45,3 +45,32 @@ void viewStudents() {
     cout << student.firstName << " " << student.lastName << " - Age: " << student.age << " - Group: " << student.group << std::endl;
     }
 }
+
+void viewClubsSocieties() {
+
+    cout << "Club/Societies Activities: Journalism Club, Red Cross Society, AISEC, Business Club, Computer Science Club" << std::endl;
+}
+
+void viewSports() {
+    cout << "Sporting Activities: Rugby, Athletics, Swimming, Soccer" << endl;
+}
+
+void viewGroupedStudents() {
+    for (const auto& student : students) {
+        cout << student.firstName << " " << student.lastName << " - Group: " << student.group << " - Sport: " << student.sport;
+        cout << " - Clubs: ";
+        for (const auto& club : student.clubs) {
+            cout << club << ", ";
+        }
+        cout << std::endl;
+    }
+}
+
+void saveDataToFile() {
+    ofstream file("student_data.csv");
+    for (const auto& student : students) {
+        file << student.firstName << "," << student.lastName << "," << student.age << "," << student.group << std::endl;
+    }
+    file.close();
+    cout << "Data saved to student_data.csv" << endl;
+}
